c6d61ec3764e794aba24fa89c2677395d59d2677
Paul Teetor <paulteetor@yahoo.com>
2025-02-18 14:47:37 -0600

    Improve strings code

--- strings.cpp
+++ strings.cpp
@@ -7,7 +7,12 @@
 
 struct String {
     uint16_t flags;
-    uint16_t length;
+    uint16_t length;      // Number of characters (following header)
+    uint16_t height;      // Height of AVL tree at this node
+    uint16_t _unused16;
+    String* left;         // Left branch of AVL tree
+    String* right;        // Right branch of AVL tree
+    String* fwd;          // Forwarding address for GC
 };
 
 const int STRINGS_SIZE = 1000;
@@ -16,15 +21,37 @@ String strings[STRINGS_SIZE];
 
 String* strings_start;
 String* strings_limit;
-String* strp;
+String* strings_free;
 
 // Utility functions
 
+static String* next_string(String* p, uint len)
+{
+  String* q = p + 1 + ((len + sizeof(String) - 1) / sizeof(String));
+  if (q < strings_limit) {
+    return q;
+  } else {
+    return (String*) 0;
+  }
+}
+
 static String* next_string(String* p)
 {
-    uint16_t len = p->length;
+  return next_string(p, p->length);
+}
 
-    return p + 1 + ((len + sizeof(String) - 1) / sizeof(String));
+static String* alloc_string(uint nChar)
+{
+  auto q = next_string(strings_free, nChar);
+  if (q == 0) {
+    // TODO: Compactify the string space; update strings_free; try alloc again
+    fatal("String space exhausted");
+  }
+
+  auto p = strings_free;
+  strings_free = q;
+
+  return p;
 }
 
 // Access functions
@@ -45,20 +72,24 @@ void initStrings()
 {
     strings_start = &strings[0];
     strings_limit = &strings[STRINGS_SIZE];
-    strp = strings_start;
+    strings_free = strings_start;
 }
 
 String* new_string(const char* str)
 {
-    String* p = strp;
+    // TODO: Check: Is string already in the AVL tree?
 
-    p->length = strlen(str);
+    auto len = strlen(str);
+    auto p = alloc_string(len);
 
-    strp = next_string(p);
-    if (strp > strings_limit) {
-        fatal("String space exhausted");
-    }
+    p->length = len;
+    memcpy(string_body(p), str, len);
 
-    memcpy(string_body(p), str, string_length(p));
+    p->height = 1;
+    p->left = 0;
+    p->right = 0;
+
+    // TODO: Perform AVL insert
+
     return p;
 }
